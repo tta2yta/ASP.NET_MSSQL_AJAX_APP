8ee8cec281177917f309247feb19b6150b950f7d
unknown <tta2yta@gmail.com>
2020-11-09 17:08:47 +0300

    adding features

--- ASP.NET_MSSQL_AJAX/Default.aspx.cs
+++ ASP.NET_MSSQL_AJAX/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +12,23 @@ namespace ASP.NET_MSSQL_AJAX
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+           
+
+        }
+
+        protected void addajax_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+
+            connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb ;User ID=sa;Password=demol23";
+
+            cnn = new SqlConnection(connetionString);
+
+            cnn.Open();
+
+            Response.Write("Connection MAde");
+            cnn.Close();
 
         }
     }
