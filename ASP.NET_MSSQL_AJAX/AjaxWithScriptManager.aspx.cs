4757b5027c69de28188a536a517506779f38ee7b
unknown <tta2yta@gmail.com>
2020-11-12 14:56:31 +0300

    adding features

--- ASP.NET_MSSQL_AJAX/AjaxWithScriptManager.aspx.cs
+++ ASP.NET_MSSQL_AJAX/AjaxWithScriptManager.aspx.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Script.Services;
 using System.Web.Services;
